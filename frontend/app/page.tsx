bf78bb323cadc841ded0bae8442278b7e85b7fc4
anasxyz <anaswessam176@gmail.com>
2025-12-17 17:55:07 +0000

    test commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,6 @@ import _button from '../components/_button';
 
 export default function Home() {
   return (
-    <_button text="Hello World" onClick={() => alert("Hello World")} />
+    <_button text="log in" onClick={() => alert("Hello World")} />
   );
 }
